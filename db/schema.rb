399378f44790aafa5ec081fd08eb5b33634dc22a
Julie Brunet <juliebrunet21@msn.com>
2023-04-27 19:10:02 +0200

    time ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_27_150149) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_27_170517) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,10 +33,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_27_150149) do
   end
 
   create_table "lessons", force: :cascade do |t|
-    t.time "occurs_on"
     t.bigint "course_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "occurs_on"
     t.index ["course_id"], name: "index_lessons_on_course_id"
   end
 
