c2952826659b11701f56a5353171a1ff18c311c2
Julie Brunet <juliebrunet21@msn.com>
2023-05-01 20:39:59 +0200

    simple_form_for en cours

--- app/controllers/absences_controller.rb
+++ app/controllers/absences_controller.rb
@@ -1,16 +1,25 @@
 class AbsencesController < ApplicationController
   def new
     @absence = Absence.new
+    @lesson = Lesson.new
   end
 
   def create
     @absence = Absence.new(absence_params)
+    @lesson = Lesson.new(lesson_params)
+
+    occurs_on = params[:lesson][:occurs_on]
+    @lesson = Lesson.find_by(course_id: current_user.course_id, occurs_on: occurs_on)
+
     @absence.user = current_user
-    if @absence.save
-      redirect_to root_path
-    else
-      render 'absences/new', status: :unprocessable_entity
-    end
+    @absence.lesson = @lesson
+    @absence.save
+
+    # if @absence.save
+    #   redirect_to root_path
+    # else
+    #   render 'absences/new', status: :unprocessable_entity
+    # end
   end
 
   def index
@@ -20,5 +29,9 @@ class AbsencesController < ApplicationController
 
   def absence_params
     params.require(:absence).permit(:user_id, :lesson_id)
+  end
+
+  def lesson_params
+    params.require(:lesson).permit(:occurs_on, :course_id)
   end
 end
