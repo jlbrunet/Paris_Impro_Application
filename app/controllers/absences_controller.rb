f35f7100c00d639cccd8bdba13cff879e0495c9b
Julie Brunet <juliebrunet21@msn.com>
2023-05-03 11:39:42 +0200

    absence se crée

--- app/controllers/absences_controller.rb
+++ app/controllers/absences_controller.rb
@@ -1,25 +1,16 @@
 class AbsencesController < ApplicationController
   def new
     @absence = Absence.new
-    @lesson = Lesson.new
   end
 
   def create
     @absence = Absence.new(absence_params)
-    @lesson = Lesson.new(lesson_params)
-
-    occurs_on = params[:lesson][:occurs_on]
-    @lesson = Lesson.find_by(course_id: current_user.course_id, occurs_on: occurs_on)
-
     @absence.user = current_user
-    @absence.lesson = @lesson
-    @absence.save
-
-    # if @absence.save
-    #   redirect_to root_path
-    # else
-    #   render 'absences/new', status: :unprocessable_entity
-    # end
+    if @absence.save
+      redirect_to root_path
+    else
+      render 'absences/new', status: :unprocessable_entity
+    end
   end
 
   def index
@@ -29,9 +20,5 @@ class AbsencesController < ApplicationController
 
   def absence_params
     params.require(:absence).permit(:user_id, :lesson_id)
-  end
-
-  def lesson_params
-    params.require(:lesson).permit(:occurs_on, :course_id)
   end
 end
