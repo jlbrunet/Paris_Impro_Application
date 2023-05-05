fd34b647dcd1ce347a4478ce2b3a4fdc4297d0e9
Julie Brunet <juliebrunet21@msn.com>
2023-05-05 15:15:44 +0200

    ok mais hash au lieu de array à résoudre

--- app/controllers/rattrapages_controller.rb
+++ app/controllers/rattrapages_controller.rb
@@ -14,11 +14,11 @@ class RattrapagesController < ApplicationController
   def create
     @occurs_on = params[:lesson][:occurs_on]
     @current_lessons = Lesson.where("DATE_TRUNC('day', occurs_on) = ?", @occurs_on.to_date)
-    redirect_to rattrapage_validation_path
+    redirect_to rattrapage_validation_path(current_lessons: @current_lessons.to_json)
   end
 
   def validation
-
+    @rattrapage_lessons = JSON.parse(params[:current_lessons])
   end
 
   def creation
