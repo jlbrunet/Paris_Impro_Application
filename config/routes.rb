cbe794a5c3364ceb369d4bbec368cf2dd1b1e5f9
Julie Brunet <juliebrunet21@msn.com>
2023-05-17 16:48:26 +0200

    boutons update course_id ok

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
     resources :lessons, only: [:index]
   end
   get '/users', to: redirect('/users/sign_up')
+  patch "profiles/:id", to: "profiles#update", as: :profile_update
 end
