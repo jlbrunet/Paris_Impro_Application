156a56b3dcfee634b49707fe4433d9025dfc647b
Julie Brunet <juliebrunet21@msn.com>
2023-06-14 15:32:47 +0200

    accès partagés admin teacher student ok

--- app/controllers/absences_controller.rb
+++ app/controllers/absences_controller.rb
@@ -1,14 +1,19 @@
 class AbsencesController < ApplicationController
   def new
-    @available_dates = []
-    @user_id = current_user.id
-    Lesson.where(course_id: current_user.course_id).each do |lesson|
-      if (Absence.where(lesson_id: lesson.id, user_id: @user_id) == []) && (((lesson.occurs_on - DateTime.now) / 3600) > 24)
-        @available_dates << lesson.occurs_on
+    @user_c = current_user
+    if @user_c.status == "teacher" || @user_c.status == "admin"
+      redirect_to courses_path
+    else
+      @available_dates = []
+      @user_id = current_user.id
+      Lesson.where(course_id: current_user.course_id).each do |lesson|
+        if (Absence.where(lesson_id: lesson.id, user_id: @user_id) == []) && (((lesson.occurs_on - DateTime.now) / 3600) > 24)
+          @available_dates << lesson.occurs_on
+        end
       end
+      @available_dates_string = @available_dates.map { |d| d.strftime("%Y-%m-%d %H:%M:%S %z") }
+      @lesson = Lesson.new
     end
-    @available_dates_string = @available_dates.map { |d| d.strftime("%Y-%m-%d %H:%M:%S %z") }
-    @lesson = Lesson.new
   end
 
   def create
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,12 +1,22 @@
 class CoursesController < ApplicationController
   def index
-    @user = current_user
-    @courses = Course.all
+    @user_c = current_user
+    if @user_c.status == "student"
+      redirect_to root_path
+    else
+      @user = current_user
+      @courses = Course.all
+    end
   end
 
   def show
-    @user = current_user
-    @course = Course.find(params[:id])
-    @students = User.where(course_id: @course.id, status: "student")
+    @user_c = current_user
+    if @user_c.status == "student"
+      redirect_to root_path
+    else
+      @user = current_user
+      @course = Course.find(params[:id])
+      @students = User.where(course_id: @course.id, status: "student")
+    end
   end
 end
