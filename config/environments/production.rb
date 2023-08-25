4254bd2980f420b2b297da40c5d4a7d66490bd4f
Julie Brunet <juliebrunet21@msn.com>
2023-08-25 11:58:23 +0200

    mailer added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,13 +65,26 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # where we should have the config to send emails (like => forgot my password)
-  config.action_mailer.default_url_options = { host: "https://rattrapage.paris-impro.com/" }
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'rattrapage@paris-impro.com'}
 
+  host = 'https://rattrapage.paris-impro.com/' #replace with your own url
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.office365.com",
+    port: 587,
+    domain: host,
+    authentication: :login,
+    enable_starttls_auto: true,
+    user_name: ENV["MY_USERNAME"],
+    password: ENV["MY_PASSWORD"]
+  }
 
+  # config.action_mailer.default_url_options = { host: "https://rattrapage.paris-impro.com/" }
+  # config.action_mailer.delivery_method = :sendmail
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default_options = {from: 'rattrapage@paris-impro.com'}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
