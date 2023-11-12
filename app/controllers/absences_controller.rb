cd279a81524a4e63e5a3ea78612ec11da2b50631
Julie Brunet <juliebrunet21@msn.com>
2023-11-12 20:04:04 +0100

    absences_controller new ok

--- app/controllers/absences_controller.rb
+++ app/controllers/absences_controller.rb
@@ -1,17 +1,11 @@
 class AbsencesController < ApplicationController
   def new
-    @user_c = current_user
-    if @user_c.status == "teacher" || @user_c.status == "admin"
+    if current_user.status == "teacher" || current_user.status == "admin"
       redirect_to courses_path
     else
-      @available_dates = []
-      @user_id = current_user.id
-      Lesson.where(course_id: current_user.course_id).each do |lesson|
-        if (Absence.where(lesson_id: lesson.id, user_id: @user_id) == []) && (((lesson.occurs_on - DateTime.now) / 3600) > 24)
-          @available_dates << lesson.occurs_on
-        end
-      end
-      @available_dates_string = @available_dates.map { |d| d.strftime("%Y-%m-%d %H:%M:%S %z") }
+      lessons_user = Lesson.where(course_id: current_user.course_id)
+      available_lessons = lessons_user.select { |lesson| absence_available?(lesson) && more_than_24h?(lesson) }
+      @available_dates = available_lessons.map { |d| d.occurs_on.strftime("%Y-%m-%d %H:%M:%S %z") }
       @lesson = Lesson.new
     end
   end
@@ -31,12 +25,16 @@ class AbsencesController < ApplicationController
   def destroy
     @absence = Absence.find(params[:id])
     @user = User.find(@absence.user_id)
-    # if @user.status == "admin"
-    #   @course = Lesson.find(@absence.lesson_id)
-    # else
-    #   @course = Course.find(@user.course_id)
-    # end
     @absence.destroy
-    # redirect_to course_lessons_path(@course)
+  end
+
+  private
+
+  def absence_available?(lesson)
+    Absence.find_by(lesson: lesson, user: current_user).nil?
+  end
+
+  def more_than_24h?(lesson)
+    ((lesson.occurs_on - DateTime.now) / 3600) > 24
   end
 end
