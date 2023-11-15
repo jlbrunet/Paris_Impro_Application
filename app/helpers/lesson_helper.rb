f06b53548b2ed9ccb04a255dd779119d20d42d07
Julie Brunet <juliebrunet21@msn.com>
2023-11-15 23:17:01 +0100

    ok controller place ouverte se supprime quand rattrapage

--- app/helpers/lesson_helper.rb
+++ app/helpers/lesson_helper.rb
@@ -5,15 +5,4 @@ module LessonHelper
     lesson_occurs_on = lesson.occurs_on.strftime("%M").to_i.positive? ? lesson_occurs_on_minutes : lesson_occurs_on_hour
     return lesson_occurs_on
   end
-
-  def new_rattrapage_takes_place_ouverte(rattrapages, absences, lesson)
-    rattrapages_and_places_ouvertes = rattrapages.select { |rattrapage| rattrapage.lesson_id == lesson.id }
-    absences_students = absences.select { |absence| absence.lesson_id == lesson.id }
-
-    rattrapages_students = rattrapages_and_places_ouvertes.select { |rattrapage| rattrapage.user.status == "student" }
-    diff = rattrapages_students.length - absences_students.length
-    diff.times { rattrapages_and_places_ouvertes_lesson.pop } if diff.positive?
-
-    return rattrapages_and_places_ouvertes
-  end
 end
