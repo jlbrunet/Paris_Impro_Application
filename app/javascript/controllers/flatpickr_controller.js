b067b60382e5787515e417cb11e144ee99def1d4
Julie Brunet <juliebrunet21@msn.com>
2023-11-18 20:48:57 +0100

    ok bug flatpickr s'affiche une fois

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -4,16 +4,19 @@ import { French } from "flatpickr/dist/l10n/fr.js"
 
 export default class extends Controller {
   static targets = ['input']
+  calendar = undefined
 
   connect() {
     const dates = JSON.parse(this.inputTarget.dataset.flatpickrAvailableDates)
     const formatted_dates = dates.map( date => date.slice(0,16) );
-
-    flatpickr(this.inputTarget, {
-      "locale": French,
-      enable: formatted_dates,
-      dateFormat: "Y-m-d H:i",
-      inline: true,
-    })
+    if (!this.calendar) {
+      console.log("ho")
+      this.calendar = flatpickr(this.inputTarget, {
+        "locale": French,
+        enable: formatted_dates,
+        dateFormat: "Y-m-d H:i",
+        inline: true,
+      })
+    }
   }
 }
