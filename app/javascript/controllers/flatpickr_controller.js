c1e48f502a7a5507bfe2a47eba9313d2ef2b260b
Julie Brunet <juliebrunet21@msn.com>
2023-11-19 22:48:20 +0100

    ok mise à jour du calendrier flatpickr

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -4,11 +4,10 @@ import { French } from "flatpickr/dist/l10n/fr.js"
 
 export default class extends Controller {
   static targets = ['input']
-  calendar = undefined
 
   connect() {
     const dates = JSON.parse(this.inputTarget.dataset.flatpickrAvailableDates)
-    this.calendar = flatpickr(this.inputTarget, {
+    flatpickr(this.inputTarget, {
       "locale": French,
       enable: dates,
       dateFormat: "Y-m-d H:i",
