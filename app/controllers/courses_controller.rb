e1e4acbb1ce8b37b9b90e4117e8f32765ad774c3
Julie Brunet <juliebrunet21@msn.com>
2024-03-28 16:43:28 +0100

    add order by day

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,15 @@
 class CoursesController < ApplicationController
   def index
     current_user.status == "student" ? (redirect_to root_path) : (@courses = Course.all)
+    @day_order = {
+      Lundi: 0,
+      Mardi: 1,
+      Mercredi: 2,
+      Jeudi: 3,
+      Vendredi: 4,
+      Samedi: 5,
+      Dimanche: 6
+    }
   end
 
   def show
