165347a6424cfb0f5bbb3342352256986f9b5ef5
Julie Brunet <juliebrunet21@msn.com>
2024-10-03 10:10:08 +0200

    add security to not create several absences

--- app/models/absence.rb
+++ app/models/absence.rb
@@ -1,4 +1,7 @@
 class Absence < ApplicationRecord
   belongs_to :user
   belongs_to :lesson
+
+  validates :lesson_id, uniqueness: { scope: :user_id,
+    message: "One absence per day per user" }
 end
