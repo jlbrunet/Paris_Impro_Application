eaf8e42c1145e515e1e941803d327dd824f742e1
Julie Brunet <juliebrunet21@msn.com>
2024-12-05 19:03:36 +0100

    fix open multiple places

--- app/models/absence.rb
+++ app/models/absence.rb
@@ -2,6 +2,8 @@ class Absence < ApplicationRecord
   belongs_to :user
   belongs_to :lesson
 
-  validates :lesson_id, uniqueness: { scope: :user_id,
-    message: "One absence per day per user" }
+  validates :lesson_id, uniqueness: {
+    scope: :user_id,
+    message: "One absence per day per user"
+  }, unless: -> { user&.status == 'admin' }
 end
