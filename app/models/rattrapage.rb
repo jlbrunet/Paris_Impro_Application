d95692b5cfa56700b20ffe0cc463145f5f9cd2bb
Julie Brunet <juliebrunet21@msn.com>
2025-03-13 17:19:12 +0100

    add uniqueness on rattrapage

--- app/models/rattrapage.rb
+++ app/models/rattrapage.rb
@@ -1,4 +1,9 @@
 class Rattrapage < ApplicationRecord
   belongs_to :user
   belongs_to :lesson
+
+  validates :lesson_id, uniqueness: {
+    scope: :user_id,
+    message: "One rattrapage per day per user"
+  }, unless: -> { user&.status == 'admin' }
 end
