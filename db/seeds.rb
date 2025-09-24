01b9a2ea6be1350c35b6aa5f5ef81ab2c20da669
Julie Brunet <juliebrunet21@msn.com>
2025-09-24 21:34:21 +0200

    change courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,47 +1,60 @@
-require "csv"
+course1 = Course.find_by({location: "Cité"})
+course1.location = "Cétaitpascité"
+course1.info = "Centre Saad Sangat 7 cour de la ferme Saint Lazare 75010 Paris"
+course1.save!
 
-User.destroy_all
-Rattrapage.destroy_all
-Absence.destroy_all
-Lesson.destroy_all
-Course.destroy_all
+course2 = Course.find_by({location: "St Paul"})
+course2.info = "Cité des arts, 18 rue de l'hôtel de ville 75004 paris métro pont marie ou st paul. STUDIO 3"
+course2.save!
 
-courses_csv_file = File.join("app/assets/data/courses.csv")
-lessons_csv_file = File.join("app/assets/data/lessons.csv")
+course3 = Course.find_by({location: "Rivoli"})
+course3.info = "Cité des arts, 18 rue de l'hôtel de ville 75004 paris métro pont marie ou st paul. STUDIO 1"
+course3.save!
 
-CSV.foreach(courses_csv_file, headers: :first_row, header_converters: :symbol) do |row|
-  row[:hour] = Time.new(2001, 1, 1, row[:hour], row[:minute], 0)
-  course = Course.new(row)
-  course.save!
-end
+# require "csv"
 
-CSV.foreach(lessons_csv_file, headers: :first_row, header_converters: :symbol) do |row|
-  occurs_on_created = Time.new(row[:occurs_on_year], row[:occurs_on_month], row[:occurs_on_day], row[:occurs_on_hour], row[:occurs_on_minutes], row[:occurs_on_seconds])
-  row[:course_id] = Course.where(location: row[:location])[0].id.to_i
-  lesson = Lesson.new(occurs_on: occurs_on_created, course_id: row[:course_id], location: row[:location])
-  lesson.save!
-end
+# User.destroy_all
+# Rattrapage.destroy_all
+# Absence.destroy_all
+# Lesson.destroy_all
+# Course.destroy_all
 
-user1 = User.new({
-  email: "ateliers@paris-impro.com", password: "123456", course_id: Course.first.id, first_name: "Professeur", last_name: "Professeur", status: "teacher"
-})
+# courses_csv_file = File.join("app/assets/data/courses.csv")
+# lessons_csv_file = File.join("app/assets/data/lessons.csv")
 
-user1.save!
+# CSV.foreach(courses_csv_file, headers: :first_row, header_converters: :symbol) do |row|
+#   row[:hour] = Time.new(2001, 1, 1, row[:hour], row[:minute], 0)
+#   course = Course.new(row)
+#   course.save!
+# end
 
-user2 = User.new({
-  email: "professeur-support@paris-impro.com", password: "123456", course_id: Course.first.id, first_name: "Professeur-support", last_name: "Professeur-support", status: "teacher"
-})
+# CSV.foreach(lessons_csv_file, headers: :first_row, header_converters: :symbol) do |row|
+#   occurs_on_created = Time.new(row[:occurs_on_year], row[:occurs_on_month], row[:occurs_on_day], row[:occurs_on_hour], row[:occurs_on_minutes], row[:occurs_on_seconds])
+#   row[:course_id] = Course.where(location: row[:location])[0].id.to_i
+#   lesson = Lesson.new(occurs_on: occurs_on_created, course_id: row[:course_id], location: row[:location])
+#   lesson.save!
+# end
 
-user2.save!
+# user1 = User.new({
+#   email: "ateliers@paris-impro.com", password: "123456", course_id: Course.first.id, first_name: "Professeur", last_name: "Professeur", status: "teacher"
+# })
 
-user3 = User.new({
-  email: "contact@paris-impro.com", password: "123456", course_id: Course.first.id, first_name: "Admin", last_name: "Admin", status: "admin"
-})
+# user1.save!
 
-user3.save!
+# user2 = User.new({
+#   email: "professeur-support@paris-impro.com", password: "123456", course_id: Course.first.id, first_name: "Professeur-support", last_name: "Professeur-support", status: "teacher"
+# })
 
-user4 = User.new({
-  email: "admin-support@paris-impro.com", password: "123456", course_id: Course.first.id, first_name: "Admin-Support", last_name: "Admin-Support", status: "admin"
-})
+# user2.save!
 
-user4.save!
+# user3 = User.new({
+#   email: "contact@paris-impro.com", password: "123456", course_id: Course.first.id, first_name: "Admin", last_name: "Admin", status: "admin"
+# })
+
+# user3.save!
+
+# user4 = User.new({
+#   email: "admin-support@paris-impro.com", password: "123456", course_id: Course.first.id, first_name: "Admin-Support", last_name: "Admin-Support", status: "admin"
+# })
+
+# user4.save!
