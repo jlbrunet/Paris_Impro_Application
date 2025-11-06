5ee46a3c7783a69f6b163798b58e03ca545fa7d0
Julie Brunet <juliebrunet21@msn.com>
2025-11-06 17:55:40 +0100

    modify petit carreaux level

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
-require "csv"
+course = Course.find_by({location: "Petits carreaux"})
+print course
+course.level = "Intermédiaire | 3"
+course.save!
+print course
+
+# require "csv"
 
 # User.destroy_all
 # Rattrapage.destroy_all
@@ -6,21 +12,21 @@ require "csv"
 # Lesson.destroy_all
 # Course.destroy_all
 
-courses_csv_file = File.join("app/assets/data/courses2.csv")
-lessons_csv_file = File.join("app/assets/data/lessons2.csv")
+# courses_csv_file = File.join("app/assets/data/courses2.csv")
+# lessons_csv_file = File.join("app/assets/data/lessons2.csv")
 
-CSV.foreach(courses_csv_file, headers: :first_row, header_converters: :symbol) do |row|
-  row[:hour] = Time.new(2001, 1, 1, row[:hour], row[:minute], 0)
-  course = Course.new(row)
-  course.save!
-end
+# CSV.foreach(courses_csv_file, headers: :first_row, header_converters: :symbol) do |row|
+#   row[:hour] = Time.new(2001, 1, 1, row[:hour], row[:minute], 0)
+#   course = Course.new(row)
+#   course.save!
+# end
 
-CSV.foreach(lessons_csv_file, headers: :first_row, header_converters: :symbol) do |row|
-  occurs_on_created = Time.new(row[:occurs_on_year], row[:occurs_on_month], row[:occurs_on_day], row[:occurs_on_hour], row[:occurs_on_minutes], row[:occurs_on_seconds])
-  row[:course_id] = Course.where(location: row[:location])[0].id.to_i
-  lesson = Lesson.new(occurs_on: occurs_on_created, course_id: row[:course_id], location: row[:location])
-  lesson.save!
-end
+# CSV.foreach(lessons_csv_file, headers: :first_row, header_converters: :symbol) do |row|
+#   occurs_on_created = Time.new(row[:occurs_on_year], row[:occurs_on_month], row[:occurs_on_day], row[:occurs_on_hour], row[:occurs_on_minutes], row[:occurs_on_seconds])
+#   row[:course_id] = Course.where(location: row[:location])[0].id.to_i
+#   lesson = Lesson.new(occurs_on: occurs_on_created, course_id: row[:course_id], location: row[:location])
+#   lesson.save!
+# end
 
 # user1 = User.new({
 #   email: "ateliers@paris-impro.com", password: "123456", course_id: Course.first.id, first_name: "Professeur", last_name: "Professeur", status: "teacher"
